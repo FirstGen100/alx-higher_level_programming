ec96d2fadcac696f6906386b8e5637ed3beaab1d
Andrew <andy.nlarry@gmail.com>
2023-08-12 01:18:01 +0300

    Update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,13 @@
 #/usr/bin/python3
-import hidden_4
+import dis
 
 if __name__ == '__main__':
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print('{:s}'.format(i))
+    module = open("hidden_4.pyc", "rb").read()
+    ins = dis.get_instructions(module)
+    names = set()
+
+    for i in ins:
+        if i.opname == "LOAD_NAME" and not i.argval.startswith("__"):
+            names.add(i.argval)
+    for name in sorted(names):
+        print(name)

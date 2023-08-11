1a51c91818d0870fe4a1c2c648f5d84093b728d8
Andrew <andy.nlarry@gmail.com>
2023-08-11 13:04:45 +0300

    update

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdbool.h>
+/**
+ * check_cycle - check if there is a cycle in the list
+ * @head: start if list
+ * Return: 1(cycle present)
+ */
+int check_cycle(struct listint_s *head)
+{
+	struct listint_s *slow = head;
+	struct listint_s *fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}

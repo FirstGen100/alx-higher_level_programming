93921a7398715b492d3eff57f31060d20c42a709
Andrew <andy.nlarry@gmail.com>
2023-08-12 01:06:26 +0300

    Update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #/usr/bin/python3
 import hidden_4
 
-name = dir(hidden_4)
-for i in name:
-    if i[:2] != '__':
-        print('{:s}'.format(i))
+if __name__ == '__main__':
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print('{:s}'.format(i))

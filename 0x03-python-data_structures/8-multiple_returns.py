4137515315010f443c482adb6a37d361a9873499
Andrew <andy.nlarry@gmail.com>
2023-08-18 09:48:12 +0300

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_string = len(sentence)
-    char_1 = sentence[0]
-    x_string = (len_string, char_1)
     if len_string == 0:
         char_1 = "None"
+        x_string = (len_string, char_1)
         return x_string
     else:
-        len_string = len(sentence)
         char_1 = sentence[0]
         return x_string

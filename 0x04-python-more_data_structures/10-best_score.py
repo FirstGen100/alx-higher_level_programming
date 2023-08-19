2ca29f1ed72376fce9569b773c292285cd218829
Andrew <andy.nlarry@gmail.com>
2023-08-19 15:54:49 +0300

    Update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,8 @@ def best_score(a_dictionary):
     max_val = float('-inf')
     m_key = None
 
-    if a_dictionary.keys() is None:
-        return 'None'
+    if a_dictionary is None or a_dictionary == {}:
+        return None
     for key, value in a_dictionary.items():
         if value > max_val:
             max_val = value

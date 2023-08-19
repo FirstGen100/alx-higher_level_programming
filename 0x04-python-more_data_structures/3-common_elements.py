4c3ad30a24c1fcefabdcd39ac00142bfe9257f0b
Andrew <andy.nlarry@gmail.com>
2023-08-19 14:49:20 +0300

    Update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for z in set_2:
-            if i == z:
-                return z
+    return set_1.intersection(set_2)

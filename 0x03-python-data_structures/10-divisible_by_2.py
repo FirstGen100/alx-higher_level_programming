ed62688a3b7eaff062cb982a189d52a5c61b4aaa
Andrew <andy.nlarry@gmail.com>
2023-08-19 11:00:49 +0300

    Update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return 'None'
+    divis = []    
+    for i in my_list:
+        if i % 2 == 0:
+            divis.append(True)
+        else:
+             divis.append(False)
+    return (divis)

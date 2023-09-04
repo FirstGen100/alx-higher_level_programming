f78e54eb6362fe0f8e527d1fd75897692ffd16e3
Andrew <andy.nlarry@gmail.com>
2023-09-04 10:29:31 +0300

    New classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Square Class
+Defines a square based in 4-square.py
+"""
+
+
+class Square:
+    """
+    defines a square
+    """
+    def __init__(self, size=0):
+        """
+        initialise the size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        get the private sze attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, width):
+        """
+        Set the value for size
+        """
+        if not isinstance(width, int):
+            raise TypeError("size must be an integer")
+        if width < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = width
+
+    def area(self):
+        """
+        find area of the square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        prints in stdout the square wuth the char # or an wmpty line
+        """
+        if self.__size < 0:
+            print('')
+        else:
+            for x in range(self.__size + 1):
+                print('###')

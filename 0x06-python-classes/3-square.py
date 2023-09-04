742e19b6d42c71161656c49b28f5eba262564fc4
Andrew <andy.nlarry@gmail.com>
2023-09-04 10:03:56 +0300

    New classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+"""Square Class
+Define a square based on 2-square.py
+"""
+
+
 class Square:
+    """
+    Define a square
+    """
     def __init__(self, size=0):
+        """
+        Initialise the size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """
+        Find the area if the square
+        """
         return (self.__size * self.__size)

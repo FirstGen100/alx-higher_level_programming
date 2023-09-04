0faf9addaa28f2fa06fb4296e19b30d74e09b038
Andrew <andy.nlarry@gmail.com>
2023-09-04 09:58:15 +0300

    New classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!usr/bin/python3
+"""Square Class
+Define a square based on 1-square.py
+"""
 class Square:
+    """
+    Define a square blueprint
+    """
     def __init__(self, size=0):
+        """Init constructor
+        Initialise square with size variable
+        Arg:
+            size: integer
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:

52bac2f4ee94e4ecaee642bf28d5d691d573ad2e
Andrew <andy.nlarry@gmail.com>
2023-09-04 11:44:57 +0300

    New classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,7 +46,8 @@ class Square:
         tuple of 2 positive integers
         """
         if (
-        not isinstance(value, tuple) or len(value) != 2
+        not isinstance(value, tuple)
+        or len(value) != 2
         or not all(isinstance(num, int) for num in value)
         or not all(num >= 0 for num in value)
         ):
@@ -63,11 +64,11 @@ class Square:
         """
         prints in stdout the square wuth the char # or an wmpty line
         """
-        if self.size == 0:
+        if self.__size == 0:
             print('')
             return
-        [print('') for i in range(0, self.position[1])]
-        for i in range(0, self.size):
-            [print(' ', end='') for x in range(0, self.position[0])]
-            [print('#', end='') for y in range(0, self.size)]
+        [print('') for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(' ', end='') for x in range(0, self.__position[0])]
+            [print('#', end='') for y in range(0, self.__size)]
             print('')

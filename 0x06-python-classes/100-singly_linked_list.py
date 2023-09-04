0dd1df8018ad6bd89de7ef395aeefc58d47b04c7
Andrew <andy.nlarry@gmail.com>
2023-09-04 13:29:58 +0300

    New classes

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,15 +5,24 @@ defines a node of a singly linked list
 
 class Node:
     def __init__(self, data, next_node=None):
+        """
+        initialise class variables
+        """
         self.__data = data
         self.__next_node = next_node
 
     @property
     def data(self):
+        """
+        get data variable
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Set the data variable
+        """
         if isinstance(value, int):
             self.__data = value
         else:
@@ -21,21 +30,38 @@ class Node:
                                                                                         
     @property
     def next_node(self):
+        """
+        get next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        set the next node variable
+        """
         if value is None or isinstance(value, Node):
             self.__next_node = value
         else:
             raise TypeError("next_node must be a Node object")
 
 
+"""SinglyLinkedList
+Creates a singly linked list
+"""
+
+
 class SinglyLinkedList:
     def __init__(self):
+        """
+        initialise the list variables
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        Insert a value at position in sorted list
+        """
         new_node = Node(value)
         if self.__head is None:
             self.__head = new_node
@@ -49,6 +75,9 @@ class SinglyLinkedList:
             new_node.next_node = current.next_node
             current.next_node = new_node
     def __str__(self):
+        """
+        convert output to string
+        """
         result = ""
         current = self.__head
         while current:

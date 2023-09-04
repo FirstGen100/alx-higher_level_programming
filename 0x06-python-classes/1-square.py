6c455a94e3fb4c7d52fd79838af55ef1d7d83367
Andrew <andy.nlarry@gmail.com>
2023-09-04 09:54:41 +0300

    New classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-class Square: 
+"""Square Class
+Defines a square based on 0-square.py
+"""
+
+
+class Square:
+    """
+    instanciate the size variable
+    """
     def __init__(self, size):
         self.__size = size

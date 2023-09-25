916dd22dd3d217d076d880d89bfa905fb6bbcac8
Andrew <andy.nlarry@gmail.com>
2023-09-26 00:37:41 +0300

    Update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -20,7 +20,6 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero")                                                                 
+        raise ZeroDivisionError("division by zero")
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
     return new_matrix
-

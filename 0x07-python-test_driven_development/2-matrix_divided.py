dd0c43be4277f251f0255f565e7822b231e4469b
Andrew <andy.nlarry@gmail.com>
2023-09-26 00:20:43 +0300

    Update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,12 +5,8 @@ divides all the elements of a matrix
 
 
 def matrix_divided(matrix, div):
-    '''
-    divides all the elements of a matrix
-    '''
-    if not isinstance(matrix, list)\
-    or not all(isinstance(row, list) for row in matrix)
-    or not all(isinstance(num, (int, float)) for row in matrix for num in row):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix)
+or not all(isinstance(num, (int, float)) for row in matrix for num in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     row_sizes = set(len(row) for row in matrix)
     if len(row_sizes) > 1:
@@ -18,6 +14,7 @@ def matrix_divided(matrix, div):
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")                                                                 
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
     return new_matrix
+

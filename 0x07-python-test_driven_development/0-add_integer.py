86bce736b40587a91981eaee0f0c24477fdeb94c
Andrew <andy.nlarry@gmail.com>
2023-09-25 11:50:06 +0300

    Update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
-
+'''
+module to add integers
+'''
 def add_integer(a, b=98):
+    '''
+    function to add integers
+    '''
     if isinstance(a, float) or isinstance(b, float):
         a = int(a)
         b = int(b)

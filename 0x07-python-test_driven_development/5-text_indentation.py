28f8b723401b015b50aa6d6248b5cc821973e702
Andrew <andy.nlarry@gmail.com>
2023-09-26 03:05:14 +0300

    Update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+prints a text with 2 new lines after the characters . , ? and :
+'''
+
+
+def  text_indentation(text):
+    '''
+    adds 2 new lines after the characters ., ? and :
+    '''
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for x in text:
+        if x == '.' or x == '?' or x == ':':
+            print(x)
+            print()
+        else:
+            print(x, end='')

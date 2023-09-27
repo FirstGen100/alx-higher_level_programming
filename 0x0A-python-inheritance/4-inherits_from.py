584bfca5fbd405bfa66f0b60a489c5926a85b9ea
Andrew <andy.nlarry@gmail.com>
2023-09-27 15:21:58 +0300

    update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''returns true if object is an isnstance of a class'''
+
+def inherits_from(obj, a_class):
+    '''returns true if object is an instance of a class'''
+
+    return isinstance(obj, a_class)

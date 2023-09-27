edd3ece8f077881a02103fc2a1d8b67063b836eb
Andrew <andy.nlarry@gmail.com>
2023-09-27 15:30:13 +0300

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''empty class'''
+
+
+class BaseGeometry:
+    '''empty area class'''
+    
+    def area(self):
+        '''returns area'''
+        raise Exception('Area() is not implemented')

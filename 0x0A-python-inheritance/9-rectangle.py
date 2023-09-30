e40d5c35538449f2cef4164da995a3a25ba32433
Andrew <andy.nlarry@gmail.com>
2023-09-30 11:16:16 +0300

    Update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return f'[Rectangle] {self.__width} / {self.__height}'
+        return f'[Rectangle] {self.__width}/{self.__height}'
 
     def __repr__(self):
         return str(self)

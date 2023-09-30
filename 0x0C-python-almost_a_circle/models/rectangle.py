01c750b3099567e309e7f1f15b831b30ae9486b6
Andrew <andy.nlarry@gmail.com>
2023-09-30 16:46:37 +0300

    Update

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+The rectangle module
+'''
+import json
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''class rectangle'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @height.setter
+    def width(self, width):
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        self.__y = y

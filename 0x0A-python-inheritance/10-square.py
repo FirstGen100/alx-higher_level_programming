5ad4745690cc46da25857852f158f21531a272b5
Andrew <andy.nlarry@gmail.com>
2023-09-30 11:26:40 +0300

    Update

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''class square'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class square'''
+
+    def __init__(self, size):
+        self.__size = size
+        super().__init__(self.__size, self.__size)

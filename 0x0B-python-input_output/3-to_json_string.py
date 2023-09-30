978ce72141225194d348b61f6f31b71bdd1e7d5d
Andrew <andy.nlarry@gmail.com>
2023-09-30 13:28:57 +0300

    Update

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+
+
+'''
+functionto return json rep of an object
+'''
+
+
+def to_json_string(myobj):
+    '''
+    json rep of a string
+    '''
+
+    return json.dumps(myobj)

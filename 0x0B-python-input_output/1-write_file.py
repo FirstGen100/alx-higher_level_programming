a81d2c5be2f2c05635cffa3f3b622700703feab0
Andrew <andy.nlarry@gmail.com>
2023-09-30 13:14:08 +0300

    Update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+write to a txt file
+'''
+
+def write_file(filename='', text=''):
+    '''
+    write content of filename to text
+    '''
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)

e786fae4f04fa9dcce230895a56131d6e7a0ab95
Andrew <andy.nlarry@gmail.com>
2023-09-30 17:57:09 +0300

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,17 @@ class Rectangle(Base):
 
     def __repr__(self):
         return str(self)
+
+    def update(self, *args):
+        '''assigns an argument to each attribute'''
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len (args) >= 5:
+            self.__y = args[4]

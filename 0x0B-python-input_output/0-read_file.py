f8006e217f50e81fa6192c8a945d8fef2e3d12dd
Andrew <andy.nlarry@gmail.com>
2023-09-30 13:09:13 +0300

    Update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''
+function to read a file
+'''
+
 
 def read_file(filename=''):
+    '''reads a file'''
+
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')

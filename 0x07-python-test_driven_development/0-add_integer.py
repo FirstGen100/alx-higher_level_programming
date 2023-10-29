4e359186fad1d87327943da364da4e288254aec3
root <root@DESKTOP-PJR1PD6.localdomain>
2023-10-29 11:06:55 +0300

    New Tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    a function to add 2 integers
+'''
+
+def add_integer(a, b=98):
+    if type(a) == float or type(a) == int:
+        a = int(a)
+    else:
+        raise TypeError('a must be an integer')
+    if type(b) == float or type(b) == int:
+        b = int(b)
+    else:
+        raise TypeError('b must be an integer')
+    return a + b
+

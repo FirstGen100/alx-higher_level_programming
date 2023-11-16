32bdc2222a911d113eccb5a1209f3edaef239c0a
Andy <andy.nlarry@gmail.com>
2023-11-17 00:08:01 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''class based on prev 5-base-geometry'''
+
+
+class BaseGeometry:
+    '''implement an empty area func'''
+    def area(self):
+        raise Exception('area() is not implemented')

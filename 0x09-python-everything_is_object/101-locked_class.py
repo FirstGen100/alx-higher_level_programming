09427ffc8418faf2d0b2e4f73eda006e148f071e
Andy <andy.nlarry@gmail.com>
2023-11-16 09:55:31 +0300

    P test

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' locked class that only allow instance attribute called first_name '''
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            self.__dict__[name] = value
+        else:
+            raise AttributeError('Cannot create instance attribute')

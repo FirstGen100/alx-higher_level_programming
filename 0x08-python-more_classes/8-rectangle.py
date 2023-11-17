caaf09983954fb74d21d556665ea77ccc91d67ed
Andy <andy.nlarry@gmail.com>
2023-11-17 16:14:54 +0300

    Inheritance

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):

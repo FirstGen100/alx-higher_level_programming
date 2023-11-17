2361a183bf9288fe5fd7fedcdb298d2d05b7e0a1
Andy <andy.nlarry@gmail.com>
2023-11-17 16:23:49 +0300

    Inheritance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -13,8 +13,18 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
-        self.__width = width
-        self.__height = height
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
 
     @property
     def width(self):

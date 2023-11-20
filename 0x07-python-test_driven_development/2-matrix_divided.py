61129aa334ff50b8ebd5d2256669a1c770f345ca
Andy <andy.nlarry@gmail.com>
2023-11-20 13:38:54 +0300

    Test development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,7 +10,7 @@ def matrix_divided(matrix, div):
         raise TypeError('div must be a number')
     if not all(isinstance(row, list) and
                all(isinstance(num, (int, float))
-                    for num in row) for row in matrix):
+                   for num in row) for row in matrix):
             raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):

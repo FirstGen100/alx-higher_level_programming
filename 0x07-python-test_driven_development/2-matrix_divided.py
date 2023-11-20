98fd94bf1588ec13c937482eab864d80a82c5e86
Andy <andy.nlarry@gmail.com>
2023-11-20 13:30:44 +0300

    Test development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,11 +8,12 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError('division by zero')
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
-    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if not all(isinstance(row, list) and all(isinstance(num, (int, float))
+        for num in row) for row in matrix):
+            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):
-        raise TypeError('Each row if the matrix must have the same size')
+        raise TypeError('Each row of the matrix must have the same size')
 
     result = [[round(num / div, 2) for num in row] for row in matrix]
 

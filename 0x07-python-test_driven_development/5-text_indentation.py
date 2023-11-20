ea45d38130b281f15b60526dedd8327e9dbeafe7
Andy <andy.nlarry@gmail.com>
2023-11-20 14:57:36 +0300

    Test development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''functioin that print text qith 2 lines after . ? : '''
+
+
+def text_indentation(text):
+    '''function that formats text'''
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    result = ''
+    for char in text:
+        if char in ['.', '?', ':']:
+            result += '\n\n'
+        else:
+            result += char
+    print(result.strip())

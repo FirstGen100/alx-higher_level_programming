57d8fb5ec839431512523bc1a5408fb462ba5687
Andy <andy.nlarry@gmail.com>
2023-11-20 15:09:33 +0300

    Test development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,11 @@ def text_indentation(text):
     '''function that formats text'''
     if not isinstance(text, str):
         raise TypeError('text must be a string')
+    text = text.strip()
     result = ''
     for char in text:
         if char in ['.', '?', ':']:
             result += '\n\n'
         else:
             result += char
-    print(result.strip())
+    print()

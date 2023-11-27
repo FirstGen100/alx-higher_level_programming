aebbf6dd6b30af224bf38e7642ec2e304e23ba06
Andy <andy.nlarry@gmail.com>
2023-11-27 16:44:16 +0300

    files

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import json
 '''function that returns json object string'''
+
+
+import json
 
 
 def to_json_string(my_obj):
     '''return json rep of an object'''
-    def to_json_string(my_obj):
-        return json.dumps(my_obj)
+    return json.dumps(my_obj)

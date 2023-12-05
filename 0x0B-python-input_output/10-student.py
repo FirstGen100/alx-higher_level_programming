6bd16b14e337c84fb838df20f3651e1dced9becc
Andy <andy.nlarry@gmail.com>
2023-12-05 17:56:46 +0300

    json

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' class student '''
+
+
+class Student:
+    '''defines a student'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        '''retrieve instance rep of Student'''
+        if attr is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attr if hasattr(self, attr)}

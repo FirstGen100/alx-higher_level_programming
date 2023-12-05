aeb469952a42960915589db050ffb9ce6b298069
Andy <andy.nlarry@gmail.com>
2023-12-05 17:08:37 +0300

    json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,11 +5,4 @@ functionreturns dictionary desc wth smp;e data struct for json srialisation'''
 
 def class_to_json(obj):
     '''return a json dictionary'''
-    my_dict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, list) or isinstance(value, dict):
-            my_dict[key] = class_to_json(value)
-        elif isinstance(value, (str, int, bool)):
-            my_dict[key] = value
-
-    return my_dict
+    return obj.__dict__

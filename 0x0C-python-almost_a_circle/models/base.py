484fb9e5ab1d77061b7ce1d0e17621061e70fa6f
Andy <andy.nlarry@gmail.com>
2023-12-05 20:02:37 +0300

    amost cirxle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,10 @@
 
 
 class Base:
+    ''' the base tracking class'''
     __nb_objects = 0
     def __init__(self, id=None):
+        '''init constructor'''
         if id is not None:
             self.id = id
         else:

4349c06863ba9623f2255bfb4d5d62d6719a27e3
Andy <andy.nlarry@gmail.com>
2023-12-06 14:54:23 +0300

    almost circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!usr/bin/python3
+'''the square class inheriting rectangle class'''
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''class square'''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        '''size getter'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''size setter'''
+        self. width = value
+        self.height = value
+
+    def __str__(self):
+        '''overwride str'''
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
+                                                 self.size)

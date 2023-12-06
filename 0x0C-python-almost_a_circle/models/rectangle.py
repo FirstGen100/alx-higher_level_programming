3b064992c66b0159078909a02fccb1d5dfa040b5
Andy <andy.nlarry@gmail.com>
2023-12-06 14:04:57 +0300

    almost circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,5 +85,5 @@ class Rectangle(Base):
 
     def __str__(self):
         '''overwride the str method'''
-        return f'[Rectangle] ({self.id})
-                 {self.__x}/{self.__y} - {self.__width}/{self.__height}'
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x,
+        self.__y, self.__width, self.__height)

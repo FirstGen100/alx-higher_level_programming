4f2e0a78264e88270c6a86c42829bad100a9aef0
Andy <andy.nlarry@gmail.com>
2023-12-06 15:27:34 +0300

    almost circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''first class base'''
 
+import json
+
 
 class Base:
     ''' the base tracking class'''
@@ -13,3 +15,11 @@ class Base:
         else:
             self.__class__. __nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''convert to json string'''
+        if list_dictionaries is None:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)

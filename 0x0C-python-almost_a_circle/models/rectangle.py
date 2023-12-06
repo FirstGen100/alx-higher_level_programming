173f901eaa3ed1f3e1968483941b5df04ef08eb6
Andy <andy.nlarry@gmail.com>
2023-12-06 13:12:40 +0300

    almost circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 '''class rectagle inheritig form base'''
 
 
-from . import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -16,32 +16,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''width getter'''
         self.__width = width
 
     @width.setter
     def width(self, value):
+        '''width setter'''
         self.__width = value
 
     @property
     def height(self):
+        '''height getter'''
         self.__height = height
 
     @height.setter
     def height(self, value):
+        '''height setter'''
         self.__height = value
 
     @property
     def x(self):
+        '''x getter'''
         self.__x = x
 
     @x.setter
     def x(self, value):
+        '''x setter'''
         self.__x = value
 
     @property
     def y(self):
+        '''y getter'''
         self.__y = y
 
     @y.setter
     def y(self, value):
+        '''y setter'''
         self.__y = value
